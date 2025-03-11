445edca7f12e3bae246d3d2539477e26557e257c
ndbn23 <ndenis183@gmail.com>
2025-03-11 19:32:42 +0200

    added player

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    game_clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
+        
+        dt = game_clock.tick(60) / 1000
+        
 if __name__ == "__main__":
     main()
